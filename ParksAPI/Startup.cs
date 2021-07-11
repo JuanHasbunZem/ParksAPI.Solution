4145492401b7d921158f05f2e81bbd4e4598bce8
JuanHasbunZem <zemenarwow@gmail.com>
2021-07-10 19:19:30 -0700

    update Startup.cs to function with Swagger

--- ParksAPI/Startup.cs
+++ ParksAPI/Startup.cs
@@ -4,7 +4,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 using ParksAPI.Models;
+
 
 namespace ParksAPI
 {
@@ -22,6 +27,21 @@ namespace ParksAPI
       services.AddDbContext<ParksAPIContext>(opt =>
         opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo 
+        { 
+          Version = "v1",
+          Title = "ParksAPI",
+          Description = "A basic API for finding parks at the state and national level",
+          Contact = new OpenApiContact
+          {
+            Name = "Juan Hasbun",
+            Email = "zemenarwow@gmail.com"
+          }
+        });
+      });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -29,6 +49,8 @@ namespace ParksAPI
       if(env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksAPI v1"));
       }
 
       app.UseRouting();
