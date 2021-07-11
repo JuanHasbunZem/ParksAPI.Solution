7439c5d05bee8bc79e5ffe1da858ed469d1792d0
JuanHasbunZem <zemenarwow@gmail.com>
2021-07-10 19:22:18 -0700

    change MySql directory to parks_api to avoid any conflicts with existing tables in previous directory

---
+++ ParksAPI/Migrations/20210711013636_Initial3.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksAPI.Migrations
+{
+    public partial class Initial3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "ParkLocation", "ParkName", "ParkType" },
+                values: new object[,]
+                {
+                    { 1, "Oregon", "Beaver Creek", "State Park" },
+                    { 2, "Oregon", "Prineville Reservior", "State Park" },
+                    { 3, "Oregon", "Willamette Mission", "State Park" },
+                    { 4, "Oregon", "Crater Lake", "National Park" },
+                    { 5, "California", "Death Valley", "National Park" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+        }
+    }
+}
