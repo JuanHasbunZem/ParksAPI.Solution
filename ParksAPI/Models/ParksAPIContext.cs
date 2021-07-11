1e9e51e050fe031159c6b84ec873abed905600e6
JuanHasbunZem <zemenarwow@gmail.com>
2021-07-10 19:21:09 -0700

    update context file for Swagger documentation

--- ParksAPI/Models/ParksAPIContext.cs
+++ ParksAPI/Models/ParksAPIContext.cs
@@ -7,7 +7,19 @@ namespace ParksAPI.Models
     public ParksAPIContext(DbContextOptions<ParksAPIContext> options) : base(options)
     {
     }
-
     public DbSet<Park> Parks { get; set; }
+
+    //Seeded Data
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Park>()
+        .HasData(
+            new Park{ParkId = 1, ParkName = "Beaver Creek", ParkLocation = "Oregon", ParkType = "State Park"},
+            new Park{ParkId = 2, ParkName = "Prineville Reservior", ParkLocation = "Oregon", ParkType = "State Park"},
+            new Park{ParkId = 3, ParkName = "Willamette Mission", ParkLocation = "Oregon", ParkType = "State Park"},
+            new Park{ParkId = 4, ParkName = "Crater Lake", ParkLocation = "Oregon", ParkType = "National Park"},
+            new Park{ParkId = 5, ParkName = "Death Valley", ParkLocation = "California", ParkType = "National Park"}
+        );
+    }
   }
 }
